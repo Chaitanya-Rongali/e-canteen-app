8dab052e61b822c22a3b2eb39d445d5121e9d4d8
chaitanya <chaitanya.rongali@everest.engineering>
2025-10-26 01:36:21 +0530

    Modifiy styles

--- src/components/MenuItem.tsx
+++ src/components/MenuItem.tsx
@@ -1,15 +1,15 @@
 import { Image, Text, View } from "react-native"
-import { styles } from "../styles/canteen-menu"
 import { menuItem } from "../types/canteen-menu"
+import { Itemstyles } from "../styles/menuItem"
 type MenuItem = {
     item: menuItem
 }
 export const MenuItem: React.FC<MenuItem> = ({ item }) => {
     return (
-        <View style={styles.item}>
-            <Image source={item.image} style={styles.image} />
-            <Text style={styles.title}>{item.name}</Text>
-            <Text style={styles.title}>₹{item.cost}</Text>
+        <View style={Itemstyles .item}>
+            <Image source={item.image} style={Itemstyles .image} />
+            <Text style={Itemstyles .title}>{item.name}</Text>
+            <Text style={Itemstyles .title}>₹{item.cost}</Text>
         </View>
     )
 }
