b667de056693344c553466042bbaa2b02f5710c5
chaitanya <chaitanya.rongali@everest.engineering>
2025-10-26 01:35:17 +0530

    Add separate styles files

---
+++ src/styles/menuItem.tsx
@@ -0,0 +1,20 @@
+import {StyleSheet} from 'react-native';
+export const Itemstyles = StyleSheet.create({
+   item: {
+    backgroundColor: 'hsla(294, 100%, 88%, 1.00)',
+    padding: 20,
+    marginVertical: 8,
+    borderRadius:10,
+    shadowColor: 'black',
+    shadowOffset: { width: 0, height: 3 },
+    shadowOpacity: 0.2,
+    shadowRadius: 5,
+  },
+  title: {
+    fontSize: 24,
+  },
+  image: {
+    height: 50,
+    width:50,
+  },
+ });
