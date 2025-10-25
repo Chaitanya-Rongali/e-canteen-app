c6f5d8e6256c49d4cd2a9e67dd2ecf7e44deae54
chaitanya <chaitanya.rongali@everest.engineering>
2025-10-25 19:38:04 +0530

    Add unit test for rendering tittle of the app

---
+++ tests/scrreens/canteen.menu.test.tsx
@@ -0,0 +1,12 @@
+import { render } from "@testing-library/react-native";
+import CanteenMenu from "../../src/screens/canteen-menu";
+
+
+describe("should dispaly the content on the homescreen", () => {
+    test("renders EverestEngineering message on the home page", async () => {
+        const { getByText } = render(<CanteenMenu/>);
+        const header = getByText('EverestEnginering');
+        expect(header).toBeTruthy()
+    });
+    
+});
