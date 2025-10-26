ceea56e6c34bb2c8e9c74ca3ff0ea54d588e8044
chaitanya <chaitanya.rongali@everest.engineering>
2025-10-27 03:28:30 +0530

    Add unit test for when add item correctly then close the modal

---
+++ tests/components/ItemModel.test.tsx
@@ -0,0 +1,30 @@
+import { fireEvent, render } from "@testing-library/react-native";
+import { ItemMOdal } from "../../src/components/ItemModal";
+import { menuSection } from "../../src/types/canteen-menu";
+import { Alert } from "react-native";
+const mockmodalVisible = true;
+const mockOnClose = jest.fn();
+const SetMenuItems = jest.fn();
+const mockTitle = "Snacks";
+const mockMenuItems: menuSection[] = [];
+describe("ItemMOdal", () => {
+    beforeEach(() => {
+        jest.clearAllMocks();
+    });
+    test("calls SetMenuItems and closes modal when valid item is added", () => {
+        const { getByText, getByPlaceholderText } = render(
+            <ItemMOdal
+                modalVisible={mockmodalVisible}
+                onClose={mockOnClose}
+                title={mockTitle}
+                SetMenuItems={SetMenuItems}
+                menuItems={mockMenuItems}
+            />
+        );
+        fireEvent.changeText(getByPlaceholderText("Item Name"), "LemonRice");
+        fireEvent.changeText(getByPlaceholderText("Price"), "60");
+        fireEvent.press(getByText("Add"));
+        expect(SetMenuItems).toHaveBeenCalled();
+        expect(mockOnClose).toHaveBeenCalled();
+    });
+})
