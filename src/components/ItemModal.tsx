8436c05ed69f78945cadae37844b21b3abc632e4
chaitanya <chaitanya.rongali@everest.engineering>
2025-10-26 19:00:24 +0530

    Add handle additem functionality

--- src/components/ItemModal.tsx
+++ src/components/ItemModal.tsx
@@ -1,21 +1,49 @@
 import { useState } from "react";
 import { Modal, Pressable, Text, TextInput, View } from "react-native";
 import { modalStyles } from "../styles/ItemModal";
-
+import { DATA } from "../data/canteen-menu";
+import { menuSection } from "../types/canteen-menu";
 
 type ItemModalprops={
     modalVisible: boolean,
     onClose: () => void;
     title: string
+   SetMenuItems: React.Dispatch<React.SetStateAction<menuSection[]>>
+   menuItems: menuSection[]
 }
 const getModalTitle = (section: string) => {
     return `Add Item to ${section}`;
 };
 
 
-export const ItemMOdal:React.FC<ItemModalprops>=({modalVisible,onClose,title })=>{
+export const ItemMOdal:React.FC<ItemModalprops>=({modalVisible,onClose,title, SetMenuItems,menuItems })=>{
     const [itemName, setItemName] = useState("");
-  const [price, setPrice] = useState("");
+    const [price, setPrice] = useState("");
+  const handleAddItem=()=>{
+    console.log('name:',itemName)
+    console.log('price:',price)
+    const newItem = {
+      id: Math.random().toString(),
+      name: itemName,
+      cost: Number(price),
+      image:''
+      
+    };
+    const updatedMenu = menuItems.map((section) => {
+      if (section.title === title) {
+        return {
+          ...section,
+          data: [...section.data, newItem],
+        };
+      }
+      return section;
+    });
+    SetMenuItems(updatedMenu);
+    setItemName("");
+    setPrice("");
+    onClose();
+}
+
     return(
         
         <Modal animationType="slide" transparent={true} visible={modalVisible}>
@@ -37,7 +65,7 @@ export const ItemMOdal:React.FC<ItemModalprops>=({modalVisible,onClose,title })=
           />
           <View style={modalStyles.modalButtons}>
             <Pressable style={modalStyles.AddButton}>
-              <Text style={modalStyles.buttonText}>Add</Text>
+              <Text style={modalStyles.buttonText} onPress={handleAddItem} >Add</Text>
             </Pressable>
             <Pressable style={modalStyles.cancelButton} onPress={onClose}>
               <Text style={modalStyles.buttonText}>Cancel</Text>
