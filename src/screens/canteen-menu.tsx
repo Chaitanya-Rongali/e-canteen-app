b8d274ec21ed0a1be216a68f2fe48f60b1f01ec8
chaitanya <chaitanya.rongali@everest.engineering>
2025-10-26 18:14:12 +0530

    Add menuItem states and sends menuitems props to the header component

--- src/screens/canteen-menu.tsx
+++ src/screens/canteen-menu.tsx
@@ -1,25 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Text, View, SectionList, Image } from 'react-native';
 import { SafeAreaView, SafeAreaProvider } from 'react-native-safe-area-context';
 import { DATA } from '../data/canteen-menu';
 import { styles } from '../styles/canteen-menu';
 import { MenuItem } from '../components/MenuItem';
 import { SectionHeader } from '../components/SectionHeader';
+import { menuSection } from '../types/canteen-menu';
 
-export const CanteenMenu = () => (
-
+export const CanteenMenu = () => {
+    const[menuItems,SetMenuItems]=useState<menuSection[]>(DATA)
+  return(
   <SafeAreaView style={styles.container} edges={['top']}>
     <Text style={styles.tittle}>EverestCanteen</Text>
     <SectionList
-      sections={DATA}
-      keyExtractor={(item, index) => item.id + index}
+      sections={menuItems}
+      keyExtractor={(item,index) => item.id+index}
       renderItem={({ item }) => <MenuItem item={item} />}
       renderSectionHeader={({ section: { title } }) => (
-        <SectionHeader title={title} />
+        <SectionHeader title={title} SetMenuItems={SetMenuItems} menuItems={menuItems}/>
       )}
     />
   </SafeAreaView>
 
-);
+)};
 
 export default CanteenMenu
