ba02b69b69b4b188a017977945d92ee74dca5e89
chaitanya <chaitanya.rongali@everest.engineering>
2025-10-26 23:33:01 +0530

    Add delteItem functionality props to the the MenuItem functionality:

--- src/components/MenuItem.tsx
+++ src/components/MenuItem.tsx
@@ -1,15 +1,22 @@
-import { Image, Text, View } from "react-native"
+import { Image, Pressable, Text, View } from "react-native"
 import { menuItem } from "../types/canteen-menu"
 import { Itemstyles } from "../styles/menuItem"
+import { modalStyles } from "../styles/ItemModal"
 type MenuItem = {
     item: menuItem
+    handleDelete: (sectionTitle: string, id: string) => void
+    sectionTitle: string;
 }
-export const MenuItem: React.FC<MenuItem> = ({ item }) => {
+export const MenuItem: React.FC<MenuItem> = ({ item,handleDelete, sectionTitle}) => {
+
     return (
-        <View style={Itemstyles .item}>
-            <Image source={{uri:item.image}} style={Itemstyles .image} />
-            <Text style={Itemstyles .title}>{item.name}</Text>
-            <Text style={Itemstyles .title}>₹{item.cost}</Text>
+        <View style={Itemstyles.item}>
+            <Image source={{ uri: item.image }} style={Itemstyles.image} />
+            <Text style={Itemstyles.title}>{item.name}</Text>
+            <Text style={Itemstyles.title}>₹{item.cost}</Text>
+            <Pressable style={modalStyles.cancelButton}onPress={() => handleDelete(sectionTitle, item.id)}>
+                <Text>Delete</Text>
+            </Pressable>
         </View>
     )
 }
