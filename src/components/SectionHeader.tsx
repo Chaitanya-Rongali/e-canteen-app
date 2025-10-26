ea6173122563dd9f69663695d474956ea79dd294
chaitanya <chaitanya.rongali@everest.engineering>
2025-10-26 19:02:07 +0530

    Add menuitem props to the header componet

--- src/components/SectionHeader.tsx
+++ src/components/SectionHeader.tsx
@@ -2,14 +2,17 @@ import { Button, Pressable, Text, View } from "react-native"
 import { headerstyles } from "../styles/sectionheader"
 import { useState } from "react"
 import { ItemMOdal } from "./ItemModal"
+import { menuSection } from "../types/canteen-menu"
 
 type sectionHeader = {
     title: string
+    SetMenuItems: React.Dispatch<React.SetStateAction<menuSection[]>>
+    menuItems: menuSection[]
 }
 
 
 
-export const SectionHeader: React.FC<sectionHeader> = ({ title }) => {
+export const SectionHeader: React.FC<sectionHeader> = ({ title ,SetMenuItems,menuItems}) => {
      const [modalVisible, setModalVisible] = useState(false);
     return (
         <View style={headerstyles.headerContainer}>
@@ -17,7 +20,7 @@ export const SectionHeader: React.FC<sectionHeader> = ({ title }) => {
             <Pressable style={headerstyles.button} onPress={()=>setModalVisible(true)}>
         <Text style={headerstyles.buttonText}>Add</Text>
       </Pressable>
-          <ItemMOdal modalVisible={modalVisible} onClose={()=>setModalVisible(false)} title={title}/>
+          <ItemMOdal modalVisible={modalVisible} onClose={()=>setModalVisible(false)} title={title} SetMenuItems={SetMenuItems} menuItems={menuItems}/>
           </View>  
       
         
