1452beb2c9b4177b2e615192eed3c2199ac71305
chaitanya <chaitanya.rongali@everest.engineering>
2025-10-27 18:29:11 +0530

    Change floder structure

--- src/screens/canteenMenu/CanteenMenu.test.tsx
+++ src/screens/canteenMenu/CanteenMenu.test.tsx
@@ -1,12 +1,10 @@
 import { render } from "@testing-library/react-native";
-import CanteenMenu from "../../src/screens/CanteenMenu";
-
-
+import CanteenMenu from "./CanteenMenu.tsx";
 describe("should dispaly the content on the homescreen", () => {
     test("renders EverestCanteen message on the canteen-menu page", () => {
-        const { getByText } = render(<CanteenMenu/>);
+        const { getByText } = render(<CanteenMenu />);
         const header = getByText('EverestCanteen');
         expect(header).toBeTruthy()
     });
-    
+
 });
