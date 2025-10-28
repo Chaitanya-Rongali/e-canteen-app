9abd42327db5a188710fcb5b7e3e59503b10b8c1
chaitanya <chaitanya.rongali@everest.engineering>
2025-10-28 23:45:52 +0530

    Resloved merge conflicts in sectionheader component

--- src/components/sectionHeader/SectionHeader.tsx
+++ src/components/sectionHeader/SectionHeader.tsx
@@ -1,16 +1,20 @@
 import { Button, Pressable, Text, View } from "react-native"
 import { headerstyles } from "./SectionHeader"
 import { useState } from "react"
-import { sectionHeader } from "../../types/CanteenMenu"
-export const SectionHeader: React.FC<sectionHeader> = ({ title }) => {
+import { ItemModal } from "/Users/chaithu/Documents/CanteenApp/src/components/itemModal/ItemModal.tsx"
+import { menuSection, sectionHeader } from "../../types/CanteenMenu"
+export const SectionHeader: React.FC<sectionHeader> = ({ title, SetMenuItems, menuItems, data }) => {
     const [modalVisible, setModalVisible] = useState(false);
     return (
         <View style={headerstyles.headerContainer}>
-            <Text testID='title' style={headerstyles.headerTitle}>{title}</Text>
+            <Text style={headerstyles.headerTitle}>{title}</Text>
+            <Text>Count:{data.length}</Text>
             <Pressable style={headerstyles.button} onPress={() => setModalVisible(true)}>
-                <Text style={headerstyles.buttonText} id="Add">Add</Text>
+                <Text style={headerstyles.buttonText}>Add</Text>
             </Pressable>
+            <ItemModal modalVisible={modalVisible} onClose={() => setModalVisible(false)} title={title} SetMenuItems={SetMenuItems} menuItems={menuItems} />
         </View>
+
 
     )
 }
