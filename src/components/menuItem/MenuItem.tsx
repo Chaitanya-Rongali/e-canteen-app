813541a17c8b6e4caa894ed1f50c9dce5ee9fa93
chaitanya <chaitanya.rongali@everest.engineering>
2025-10-28 23:44:20 +0530

    Resolved conflicts in the menutem component

--- src/components/menuItem/MenuItem.tsx
+++ src/components/menuItem/MenuItem.tsx
@@ -1,12 +1,16 @@
-import { Image, Text, View } from "react-native"
+import { Image, Pressable, Text, View } from "react-native"
 import { Itemstyles } from "./MenuItem"
-import { MenuItemProps } from "../../types/CanteenMenu"
-export const MenuItem: React.FC<MenuItemProps> = ({ item }) => {
+import { modalStyles } from "../itemModal/ItemModal"
+import { MenuItemprops } from "../../types/CanteenMenu"
+export const MenuItem: React.FC<MenuItemprops> = ({ item, handleDelete, sectionTitle }) => {
     return (
         <View style={Itemstyles.item}>
-            <Image source={item.image} style={Itemstyles.image} />
-            <Text testID="name" style={Itemstyles.title}>{item.name}</Text>
-            <Text testID="cost" style={Itemstyles.title}>₹{item.cost}</Text>
+            <Image source={{ uri: item.image }} style={Itemstyles.image} />
+            <Text style={Itemstyles.title}>{item.name}</Text>
+            <Text style={Itemstyles.title}>₹{item.cost}</Text>
+            <Pressable style={modalStyles.cancelButton} onPress={() => handleDelete(sectionTitle, item.id)}>
+                <Text>Delete</Text>
+            </Pressable>
         </View>
     )
 }
