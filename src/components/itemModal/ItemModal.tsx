ca9c1e8a869fa50f5bad501751232e65c4f316b2
chaitanya <chaitanya.rongali@everest.engineering>
2025-10-28 23:42:45 +0530

    Modify the floder structure

--- src/components/itemModal/ItemModal.tsx
+++ src/components/itemModal/ItemModal.tsx
@@ -1,25 +1,22 @@
 import { useState } from "react";
 import { Alert, Modal, Pressable, Text, TextInput, View } from "react-native";
-import { modalStyles } from "../styles/ItemModal";
-import { ItemModalprops } from "../types/canteen-menu";
-
+import { modalStyles } from "./ItemModal";
+import { ItemModalprops } from "../../types/CanteenMenu";
 
 const getModalTitle = (section: string) => {
-    return `Add Item to ${section}`;
+  return `Add Item to ${section}`;
 };
-
-
-export const ItemMOdal:React.FC<ItemModalprops>=({modalVisible,onClose,title, SetMenuItems,menuItems })=>{
-    const [itemName, setItemName] = useState("");
-    const [price, setPrice] = useState("");
-  const handleAddItem=(title:string)=>{
-    console.log('name:',itemName)
-    console.log('price:',price)
-    if(!price || !itemName){
+export const ItemModal: React.FC<ItemModalprops> = ({ modalVisible, onClose, title, SetMenuItems, menuItems }) => {
+  const [itemName, setItemName] = useState("");
+  const [price, setPrice] = useState("");
+  const handleAddItem = (title: string) => {
+    console.log('name:', itemName)
+    console.log('price:', price)
+    if (!price || !itemName) {
       Alert.alert('Please Enter all values')
       return
     }
-    if(Number(price)<0 || isNaN(Number(price))){
+    if (Number(price) < 0 || isNaN(Number(price))) {
       Alert.alert("Amount must be postive")
       return
     }
@@ -27,8 +24,8 @@ export const ItemMOdal:React.FC<ItemModalprops>=({modalVisible,onClose,title, Se
       id: Math.random().toString(),
       name: itemName,
       cost: Number(price),
-      image:''
-      
+      image: ''
+
     };
     const updatedMenu = menuItems.map((section) => {
       if (section.title === title) {
@@ -43,15 +40,15 @@ export const ItemMOdal:React.FC<ItemModalprops>=({modalVisible,onClose,title, Se
     setItemName("");
     setPrice("");
     onClose();
-}
+  }
 
-    return(
-        
-        <Modal  testID="modal" animationType="slide" transparent={true} visible={modalVisible}>
-          <View style={modalStyles.modal}>
+  return (
+
+    <Modal testID="modal" animationType="slide" transparent={true} visible={modalVisible}>
+      <View style={modalStyles.modal}>
         <View style={modalStyles.modalContainer}>
-        <Text style={modalStyles.modalTitle}>{getModalTitle(title)}</Text>
-            <TextInput
+          <Text style={modalStyles.modalTitle}>{getModalTitle(title)}</Text>
+          <TextInput
             style={modalStyles.input}
             placeholder="Item Name"
             value={itemName}
@@ -66,17 +63,17 @@ export const ItemMOdal:React.FC<ItemModalprops>=({modalVisible,onClose,title, Se
           />
           <View style={modalStyles.modalButtons}>
             <Pressable style={modalStyles.AddButton}>
-              <Text style={modalStyles.buttonText} onPress={()=>{handleAddItem(title)}} >Add</Text>
+              <Text style={modalStyles.buttonText} onPress={() => { handleAddItem(title) }} >Add</Text>
             </Pressable>
             <Pressable style={modalStyles.cancelButton} onPress={onClose}>
               <Text style={modalStyles.buttonText}>Cancel</Text>
             </Pressable>
-            </View>   
-            </View> 
-            </View> 
-        
-        </Modal>
-        
-        
-    )
+          </View>
+        </View>
+      </View>
+
+    </Modal>
+
+
+  )
 }
