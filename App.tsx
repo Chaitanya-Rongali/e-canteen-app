8f158b364932c125b92cd3ed59a5daf382f40b8d
chaitanya <chaitanya.rongali@everest.engineering>
2025-10-28 11:48:57 +0530

    Fix the path issue

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import { SafeAreaProvider, } from 'react-native-safe-area-context';
-import CanteenMenu from './src/screens/canteenMenu/CanteenMenu';
+import CanteenMenu from './src/screens/canteenMenu/CanteenMenu.tsx';
 export default function App() {
   return (
     <SafeAreaProvider>
