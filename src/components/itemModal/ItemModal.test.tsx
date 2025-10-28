bb67f7b1ea62e068a33358d9cb162daf9be4506c
chaitanya <chaitanya.rongali@everest.engineering>
2025-10-28 23:49:26 +0530

    Rename file name and change the floder structure

--- src/components/itemModal/ItemModal.test.tsx
+++ src/components/itemModal/ItemModal.test.tsx
@@ -1,7 +1,8 @@
 import { fireEvent, render } from "@testing-library/react-native";
-import { ItemMOdal } from "../../src/components/ItemModal";
-import { menuSection } from "../../src/types/canteen-menu";
+import React from "react";
+import { ItemModal } from '/Users/chaithu/Documents/CanteenApp/src/components/itemModal/ItemModal.tsx'
 import { Alert } from "react-native";
+import { menuSection } from "../../types/CanteenMenu";
 const mockmodalVisible = true;
 const mockOnClose = jest.fn();
 const SetMenuItems = jest.fn();
@@ -13,7 +14,7 @@ describe("ItemMOdal", () => {
     });
     test("calls SetMenuItems and closes modal when valid item is added", () => {
         const { getByText, getByPlaceholderText } = render(
-            <ItemMOdal
+            <ItemModal
                 modalVisible={mockmodalVisible}
                 onClose={mockOnClose}
                 title={mockTitle}
@@ -30,7 +31,7 @@ describe("ItemMOdal", () => {
     test("displays alert if fields are empty", () => {
         const alertSpy = jest.spyOn(Alert, "alert");
         const { getByText } = render(
-            <ItemMOdal
+            <ItemModal
                 modalVisible={mockmodalVisible}
                 onClose={mockOnClose}
                 title={mockTitle}
@@ -43,38 +44,38 @@ describe("ItemMOdal", () => {
         expect(alertSpy).toHaveBeenCalledWith("Please Enter all values");
     });
     test("displays alert if price is negative", () => {
-    const alertSpy = jest.spyOn(Alert, "alert");
-    const { getByText, getByPlaceholderText } = render(
-        <ItemMOdal
-        modalVisible={mockmodalVisible}
-        onClose={mockOnClose}
-        title={mockTitle}
-        SetMenuItems={SetMenuItems}
-        menuItems={mockMenuItems}
-      />
-      
-    );
-    fireEvent.changeText(getByPlaceholderText("Item Name"), "firedrice");
-    fireEvent.changeText(getByPlaceholderText("Price"), "-60");
-    fireEvent.press(getByText("Add"));
-    expect(alertSpy).toHaveBeenCalledWith("Amount must be postive");
-  });
-  test("modal close when click the cancel", () => {
+        const alertSpy = jest.spyOn(Alert, "alert");
+        const { getByText, getByPlaceholderText } = render(
+            <ItemModal
+                modalVisible={mockmodalVisible}
+                onClose={mockOnClose}
+                title={mockTitle}
+                SetMenuItems={SetMenuItems}
+                menuItems={mockMenuItems}
+            />
 
-    const { getByText, getByPlaceholderText } = render(
-        <ItemMOdal
-        modalVisible={mockmodalVisible}
-        onClose={mockOnClose}
-        title={mockTitle}
-        SetMenuItems={SetMenuItems}
-        menuItems={mockMenuItems}
-      />
-    );
-    fireEvent.press(getByText("Cancel"));
-     expect(mockOnClose).toHaveBeenCalled();
-    
-   
-  });
+        );
+        fireEvent.changeText(getByPlaceholderText("Item Name"), "firedrice");
+        fireEvent.changeText(getByPlaceholderText("Price"), "-60");
+        fireEvent.press(getByText("Add"));
+        expect(alertSpy).toHaveBeenCalledWith("Amount must be postive");
+    });
+    test("modal close when click the cancel", () => {
+
+        const { getByText, getByPlaceholderText } = render(
+            <ItemModal
+                modalVisible={mockmodalVisible}
+                onClose={mockOnClose}
+                title={mockTitle}
+                SetMenuItems={SetMenuItems}
+                menuItems={mockMenuItems}
+            />
+        );
+        fireEvent.press(getByText("Cancel"));
+        expect(mockOnClose).toHaveBeenCalled();
+
+
+    });
 
 
 })
