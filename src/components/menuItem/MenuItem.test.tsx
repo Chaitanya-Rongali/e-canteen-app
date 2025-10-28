6e61f685d2f55dca70cb436a89d5b13fcb27fab3
chaitanya <chaitanya.rongali@everest.engineering>
2025-10-28 23:43:43 +0530

    Resolved merge conflicts

--- src/components/menuItem/MenuItem.test.tsx
+++ src/components/menuItem/MenuItem.test.tsx
@@ -1,6 +1,8 @@
-import { render } from "@testing-library/react-native";
-import { MenuItem } from "./MenuItem.tsx";
+import { fireEvent, render } from "@testing-library/react-native";
+import { MenuItem } from '/Users/chaithu/Documents/CanteenApp/src/components/menuItem/MenuItem.tsx'
 import { menuItem } from "../../types/CanteenMenu";
+const handleDelete = jest.fn();
+const sectionTitle: string = ''
 const mockItem: menuItem = {
     id: '1',
     name: "Idly",
@@ -9,10 +11,17 @@ const mockItem: menuItem = {
 }
 describe('MenuItem', () => {
     test('should idly and cost on the canteen-menu page screen', () => {
-        const { getByTestId, getByText } = render(<MenuItem item={mockItem} />)
+        const { getByTestId, getByText } = render(<MenuItem item={mockItem} handleDelete={handleDelete} sectionTitle={sectionTitle} />)
         const itemName = getByText('Idly')
         expect(itemName).toBeOnTheScreen();
         const itemCost = getByText('₹30')
         expect(itemCost).toBeOnTheScreen();
+    })
+    test('When click the delete button then menuitem will be deleted', () => {
+        const { getByText } = render(< MenuItem item={mockItem} handleDelete={handleDelete} sectionTitle={sectionTitle} />)
+        const deletButton = getByText('Delete')
+        fireEvent.press(deletButton)
+        expect(handleDelete).toHaveBeenCalled();
+
     })
 })
