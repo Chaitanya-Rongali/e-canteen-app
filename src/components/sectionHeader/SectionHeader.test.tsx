0fc520516e3b465ffc2d3cc2801346ba2e416669
chaitanya <chaitanya.rongali@everest.engineering>
2025-10-28 23:45:13 +0530

    Resloved merge conflicts in sectionheader test component

--- src/components/sectionHeader/SectionHeader.test.tsx
+++ src/components/sectionHeader/SectionHeader.test.tsx
@@ -1,15 +1,36 @@
-import { render } from "@testing-library/react-native";
-import { SectionHeader } from "./SectionHeader.tsx";
+import { fireEvent, render, screen } from "@testing-library/react-native";
+import { SectionHeader } from "/Users/chaithu/Documents/CanteenApp/src/components/sectionHeader/SectionHeader.tsx";
+import { menuSection } from "../../types/CanteenMenu.ts";
 const mocktittle = 'Breakfast'
+const mockSetMenuItems = jest.fn();
+const mockMenuItems: menuSection[] = [];
+const mockData: any = [];
+const mockTitle = "EverestCanteen";
+
 describe("SectionHeader ", () => {
     test('Should Add button on the screen', () => {
-        const { getByText } = render(<SectionHeader title={mocktittle} />)
+        const { getByText } = render(<SectionHeader title={mocktittle} SetMenuItems={mockSetMenuItems} menuItems={mockMenuItems} data={mockData} />)
         const addButton = getByText('Add')
         expect(addButton).toBeTruthy();
     })
     test('should Breakfast on the screen', () => {
-        const { getByText } = render(<SectionHeader title={mocktittle} />)
+        const { getByText } = render(<SectionHeader title={mocktittle} SetMenuItems={mockSetMenuItems} menuItems={mockMenuItems}
+            data={mockData} />)
         const tittle = getByText('Breakfast')
         expect(tittle).toBeOnTheScreen();
+    })
+    test("should text and add button on the screen", () => {
+        const { getByText } = render(<SectionHeader title={mockTitle} SetMenuItems={mockSetMenuItems} menuItems={mockMenuItems}
+            data={mockData} />)
+        expect(getByText("Add")).toBeTruthy();
+        expect(getByText("EverestCanteen")).toBeTruthy();
+    })
+    test("should open the modal when click the add button", () => {
+        const { getByText } = render(<SectionHeader title={mockTitle} SetMenuItems={mockSetMenuItems} menuItems={mockMenuItems}
+            data={mockData} />)
+        expect(screen.queryByTestId('modal')).toBeNull();
+        const addButton = getByText("Add");
+        fireEvent.press(addButton);
+        expect(screen.getByTestId('modal')).toBeVisible();
     })
 })
