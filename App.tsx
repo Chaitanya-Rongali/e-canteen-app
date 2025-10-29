921a0f7a31e5b5181d61dd4b0524472b393b4a1a
chaitanya <chaitanya.rongali@everest.engineering>
2025-10-29 14:37:36 +0530

    Modified file names in the component

--- App.tsx
+++ App.tsx
@@ -1,5 +1,4 @@
 import { SafeAreaProvider, } from 'react-native-safe-area-context';
-import { CanteenMenu } from './src/screens/canteenMenu/CanteenMenu.tsx';
 import HomeNavigation from './src/navigation/TabNavigation.tsx';
 export default function App() {
   return (
--- src/screens/canteenMenu/CanteenMenu.test.tsx
+++ src/screens/canteenMenu/CanteenMenu.test.tsx
@@ -1,8 +1,8 @@
 import { render } from "@testing-library/react-native";
-import {CanteenMenu} from "./CanteenMenu.tsx";
+import {CanteenMenuScreen} from "./CanteenMenu.tsx";
 describe("should dispaly the content on the homescreen", () => {
     test("renders EverestCanteen message on the canteen-menu page", () => {
-        const { getByText } = render(<CanteenMenu />);
+        const { getByText } = render(<CanteenMenuScreen />);
         const header = getByText('Everest-Canteen👨‍🍳');
         expect(header).toBeTruthy()
     });
