6cdabd48f22680eded9072d06ac4882b2b642f92
chaitanya <chaitanya.rongali@everest.engineering>
2025-10-30 23:09:05 +0530

    Add loginscreen

---
+++ src/screens/LoginScreen/LoginScreen.tsx
@@ -0,0 +1,34 @@
+import { Button, Text, TextInput, TouchableOpacity, View } from "react-native"
+import { loginScreenStyles } from "./LoginScreen"
+import React from "react"
+import { ParamListBase, useNavigation } from "@react-navigation/native";
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
+
+
+export const LoginScreen = () => {
+       const navigation = useNavigation<NativeStackNavigationProp<ParamListBase>>();
+    return (
+        <View style={loginScreenStyles.container}>
+            <Text style={loginScreenStyles.title}> Sign in your account</Text>
+            <Text style={loginScreenStyles.labelText}>User name:</Text>
+            <View style={loginScreenStyles.inputView}>
+                <TextInput placeholder="Please enter Username" />
+            </View>
+            <Text style={loginScreenStyles.labelText}>Password:</Text>
+            <View style={loginScreenStyles.inputView}>
+                <TextInput secureTextEntry placeholder="*********" />
+            </View>
+            <TouchableOpacity style={loginScreenStyles.loginBtn}>
+                <Text>SIGN IN </Text>
+            </TouchableOpacity>
+            <Text> or sign in with</Text>
+            <Text>Don't have an account?</Text>
+            <Button title="Sign Up" onPress={() => navigation.navigate('RegistrationScreen')} />
+
+
+
+
+
+        </View>
+    )
+}
