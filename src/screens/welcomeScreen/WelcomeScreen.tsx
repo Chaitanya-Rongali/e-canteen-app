3674cafb2ac81d697ce08244124e9082a65268bd
chaitanya <chaitanya.rongali@everest.engineering>
2025-10-30 23:08:04 +0530

    Add welcome screen

---
+++ src/screens/welcomeScreen/WelcomeScreen.tsx
@@ -0,0 +1,23 @@
+import { Button, Pressable, Text, TouchableOpacity, View } from "react-native"
+import { welcomeScreenStyles } from "./WelcomeScreen";
+import { ParamListBase, useNavigation } from "@react-navigation/native";
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
+
+
+export const WelcomeScreen = () => {
+    const navigation = useNavigation<NativeStackNavigationProp<ParamListBase>>();
+    return (
+        
+        <View style={welcomeScreenStyles.container}>
+            <Text>A CANTEEN JUST FOR YOU</Text>
+            <Pressable style={welcomeScreenStyles.button} onPress={() => navigation.navigate('LoginScreen')} >
+                <Text style={welcomeScreenStyles.buttonText}>LOGIN AS USER</Text>
+            </Pressable>
+            <TouchableOpacity style={welcomeScreenStyles.button} onPress={() => navigation.navigate('LoginScreen')}>
+                <Text style={welcomeScreenStyles.buttonText}>LOGIN AS ADMIN</Text>
+            </TouchableOpacity>
+            <Text>Welcome to Everest Canteen</Text>
+        </View>
+ 
+    )
+}
