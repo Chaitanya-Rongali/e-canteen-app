ef08dcce031dff324e4d955106a62ab7b9098275
chaitanya <chaitanya.rongali@everest.engineering>
2025-10-31 10:38:21 +0530

    Sent role params through navigation

--- src/screens/welcomeScreen/WelcomeScreen.tsx
+++ src/screens/welcomeScreen/WelcomeScreen.tsx
@@ -3,21 +3,22 @@ import { welcomeScreenStyles } from "./WelcomeScreen";
 import { ParamListBase, useNavigation } from "@react-navigation/native";
 import { NativeStackNavigationProp } from "@react-navigation/native-stack";
 
-
 export const WelcomeScreen = () => {
     const navigation = useNavigation<NativeStackNavigationProp<ParamListBase>>();
+    const handleLoginScreen = (role: 'user' | 'admin') => {
+        navigation.navigate('LoginScreen', { role })
+    }
     return (
-        
         <View style={welcomeScreenStyles.container}>
             <Text>A CANTEEN JUST FOR YOU</Text>
-            <Pressable style={welcomeScreenStyles.button} onPress={() => navigation.navigate('LoginScreen')} >
+            <Pressable style={welcomeScreenStyles.button} onPress={() => handleLoginScreen('user')} >
                 <Text style={welcomeScreenStyles.buttonText}>LOGIN AS USER</Text>
             </Pressable>
-            <TouchableOpacity style={welcomeScreenStyles.button} onPress={() => navigation.navigate('LoginScreen')}>
+            <TouchableOpacity style={welcomeScreenStyles.button} onPress={() => handleLoginScreen('admin')}>
                 <Text style={welcomeScreenStyles.buttonText}>LOGIN AS ADMIN</Text>
             </TouchableOpacity>
             <Text>Welcome to Everest Canteen</Text>
         </View>
- 
+
     )
 }
