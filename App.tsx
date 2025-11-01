ed06d8dd7c74b8c51abcff7fedbfdb8d2389e35d
chaitanya <chaitanya.rongali@everest.engineering>
2025-11-01 17:21:17 +0530

    Add Stack navigation component

--- App.tsx
+++ App.tsx
@@ -1,9 +1,10 @@
 import { SafeAreaProvider, } from 'react-native-safe-area-context';
 import HomeNavigation from './src/navigation/TabNavigation.tsx';
+import { StackNavigation } from './src/navigation/StackNavigation.tsx';
 export default function App() {
   return (
     <SafeAreaProvider>
-      < HomeNavigation/>
+      <StackNavigation/>
     </SafeAreaProvider>
   );
 }
