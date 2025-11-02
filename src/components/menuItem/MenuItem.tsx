bc0f6be53441b5b85c7f07cd9b43c57a19d14b3c
chaitanya <chaitanya.rongali@everest.engineering>
2025-11-02 14:24:17 +0530

    Add role props for base one rolr display menuItem card

--- src/components/menuItem/MenuItem.tsx
+++ src/components/menuItem/MenuItem.tsx
@@ -2,15 +2,17 @@ import { Image, Pressable, Text, View } from "react-native"
 import { Itemstyles } from "./MenuItem"
 import { modalStyles } from "../itemModal/ItemModal"
 import { MenuItemprops } from "../../types/CanteenMenu"
-export const MenuItem: React.FC<MenuItemprops> = ({ item, handleDelete, sectionTitle }) => {
+export const MenuItem: React.FC<MenuItemprops> = ({ item, handleDelete, sectionTitle,role }) => {
     return (
         <View style={Itemstyles.item}>
             <Image source={{ uri: item.image }} style={Itemstyles.image} />
             <Text style={Itemstyles.title}>{item.name}</Text>
             <Text style={Itemstyles.title}>₹{item.cost}</Text>
-            <Pressable style={modalStyles.cancelButton} onPress={() => handleDelete(sectionTitle, item.id)}>
+            {role==="Admin" ?<Pressable style={modalStyles.cancelButton} onPress={() => handleDelete(sectionTitle, item.id)}>
                 <Text>Delete</Text>
-            </Pressable>
+            </Pressable>:<Pressable style={modalStyles.cancelButton} onPress={() => handleDelete(sectionTitle, item.id)}>
+                <Text>Add</Text>
+            </Pressable>}
         </View>
     )
 }
