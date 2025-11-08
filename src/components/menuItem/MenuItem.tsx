59a02370546c0c827530f0c1534ecc233d8f4cf0
chaitanya <chaitanya.rongali@everest.engineering>
2025-11-08 22:40:57 +0530

    Add custom button to the components

--- src/components/menuItem/MenuItem.tsx
+++ src/components/menuItem/MenuItem.tsx
@@ -2,17 +2,15 @@ import { Image, Pressable, Text, View } from "react-native"
 import { Itemstyles } from "./MenuItem"
 import { modalStyles } from "../itemModal/ItemModal"
 import { MenuItemprops } from "../../types/CanteenMenu"
+import { CustomButton } from "../Button/CustomButton"
 export const MenuItem: React.FC<MenuItemprops> = ({ item, handleDelete, sectionTitle,role }) => {
     return (
         <View style={Itemstyles.item}>
             <Image source={{ uri: item.image }} style={Itemstyles.image} />
             <Text style={Itemstyles.title}>{item.name}</Text>
             <Text style={Itemstyles.title}>₹{item.cost}</Text>
-            {role==="admin" ?<Pressable style={modalStyles.cancelButton} onPress={() => handleDelete(sectionTitle, item.id)}>
-                <Text>Delete</Text>
-            </Pressable>:<Pressable style={modalStyles.cancelButton} onPress={() => handleDelete(sectionTitle, item.id)}>
-                <Text>Add</Text>
-            </Pressable>}
+            {role==="admin" ?<CustomButton style={modalStyles.cancelButton} onPress={() => handleDelete(sectionTitle, item.id)} title={"Delete"}/>:
+            <CustomButton style={modalStyles.cancelButton} onPress={() => handleDelete(sectionTitle, item.id)} title={"Add"}/>}
         </View>
     )
 }
