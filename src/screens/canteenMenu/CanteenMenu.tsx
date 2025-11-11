4d14634eb4b7b4166705b5fa53df6990905c3259
chaitanya <chaitanya.rongali@everest.engineering>
2025-11-11 11:06:25 +0530

    Modify the getcanteen menu items functionality

--- src/screens/canteenMenu/CanteenMenu.tsx
+++ src/screens/canteenMenu/CanteenMenu.tsx
@@ -1,27 +1,42 @@
 import React, { useEffect, useState } from 'react';
 import { Text, View, SectionList, Image } from 'react-native';
 import { SafeAreaView, SafeAreaProvider } from 'react-native-safe-area-context';
-import { DATA } from '../../data/CanteenMenu';
+// import { DATA } from '../../data/CanteenMenu';
 import { styles } from './CanteenMenu';
-import { menuSection } from '../../types/CanteenMenu.ts';
+import { menuItem, menuSection } from '../../types/CanteenMenu.ts';
 import { MenuItem } from '../../components/menuItem/MenuItem.tsx';
 import { SectionHeader } from '../../components/sectionHeader/SectionHeader.tsx';
 import { deleteMenuItem, fetchMenuItems } from '../../server.ts';
 
 
-export const CanteenMenuScreen:React.FC = () => {
-  const [menuItems, SetMenuItems] = useState<menuSection[]>(DATA)
-
+export const CanteenMenuScreen: React.FC = () => {
+  const [menuItems, SetMenuItems] = useState<menuSection[]>([])
   useEffect(() => {
-    const getMenuItems= async()=>{
-    try{
-   const data= await fetchMenuItems()
-   SetMenuItems(data);
- }catch{
-      console.error('Error fetching menu items');
-}
-  };
-},[]);
+    const getMenuItems = async () => {
+      try {
+        const data = await fetchMenuItems();
+        const grouped: { [key: string]: menuItem[] } = {};
+        data.forEach((item: menuItem) => {
+          if (!grouped[item.sectionName]) {
+            grouped[item.sectionName] = [];
+          }
+          grouped[item.sectionName].push(item);
+        });
+
+        const sections: menuSection[] = Object.keys(grouped).map(title => ({
+          title,
+          data: grouped[title],
+        }));
+        SetMenuItems(sections);
+
+      } catch {
+        console.error('Error fetching menu items');
+      }
+    };
+    getMenuItems();
+  }, []);
+
+
   const handleDelete = async (sectionTitle: string, id: string) => {
     await deleteMenuItem(id)
     const updatedSections = menuItems.map(section => {
@@ -35,15 +50,14 @@ export const CanteenMenuScreen:React.FC = () => {
     });
     SetMenuItems(updatedSections);
   }
-  
 
-  
+
   return (
     <SafeAreaView style={styles.container} edges={['top']}>
       <Text style={styles.tittle}>Everest-Canteen👨‍🍳</Text>
       <SectionList
         sections={menuItems}
-        keyExtractor={(item, index) => item.id + index}
+        keyExtractor={(item, index) => index.toString()}
         renderItem={({ item, section }) => <MenuItem item={item} handleDelete={handleDelete} sectionTitle={section.title} role={'admin'} />}
         renderSectionHeader={({ section }) => (
           <SectionHeader title={section.title} SetMenuItems={SetMenuItems} menuItems={menuItems} data={section.data} role={'admin'} />
