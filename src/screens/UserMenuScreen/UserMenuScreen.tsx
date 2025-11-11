73098ba2d7248062b7f71aad3520f1cd07f1c215
chaitanya <chaitanya.rongali@everest.engineering>
2025-11-11 11:41:13 +0530

    Add logout functionality

--- src/screens/UserMenuScreen/UserMenuScreen.tsx
+++ src/screens/UserMenuScreen/UserMenuScreen.tsx
@@ -4,23 +4,29 @@ import { loginScreenStyles } from "../LoginScreen/LoginScreen"
 import { MenuItem } from "../../components/menuItem/MenuItem.tsx"
 import { SetStateAction, useState } from "react"
 import { menuSection } from "../../types/CanteenMenu.ts"
-import { DATA } from "../../data/CanteenMenu.ts"
+// import { DATA } from "../../data/CanteenMenu.ts"
 import { SectionHeader } from "../../components/sectionHeader/SectionHeader.tsx"
 import { UserMenuStyles } from "./UserMenuScreenStyles.ts"
+import { ParamListBase, useNavigation } from "@react-navigation/native"
+import { NativeStackNavigationProp } from "@react-navigation/native-stack"
 
 export const UserMenuScreen = () => {
-    const [menuItems] = useState<menuSection[]>(DATA)
+    const [menuItems] = useState<menuSection[]>([])
+    const navigation = useNavigation<NativeStackNavigationProp<ParamListBase>>();
     const handleDelete=()=>{
       handleDelete()
     }
     const SetMenuItems=()=>{
         SetMenuItems()
     }
+    const hanldeLogout=()=>{
+       navigation.navigate('WelcomeScreen')
+    }
     return (
         <View>
             <View style={UserMenuStyles.headerContainer}>
                 <Text style={UserMenuStyles.headerTitle}>Our Menu</Text>
-                <TouchableOpacity style={UserMenuStyles.button}>
+                <TouchableOpacity style={UserMenuStyles.button} onPress={hanldeLogout}>
                     <Text style={UserMenuStyles.buttonText}>LogOut</Text>
                 </TouchableOpacity>
             </View>
