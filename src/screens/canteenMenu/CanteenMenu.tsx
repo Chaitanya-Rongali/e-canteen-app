2e6aacd2713b3483744b9fea0f71227b043caa76
chaitanya <chaitanya.rongali@everest.engineering>
2025-11-12 17:59:03 +0530

    Add adminprofile perimisions functionality and adminprofile button

--- src/screens/canteenMenu/CanteenMenu.tsx
+++ src/screens/canteenMenu/CanteenMenu.tsx
@@ -1,15 +1,19 @@
 import React, { useState } from 'react';
-import { Text, View, SectionList, Image } from 'react-native';
+import { Text, View, SectionList, Image, Alert, Pressable } from 'react-native';
 import { SafeAreaView, SafeAreaProvider } from 'react-native-safe-area-context';
 import { DATA } from '../../data/CanteenMenu';
 import { styles } from './CanteenMenu';
 import { menuSection } from '../../types/CanteenMenu.ts';
 import { MenuItem } from '../../components/menuItem/MenuItem.tsx';
 import { SectionHeader } from '../../components/sectionHeader/SectionHeader.tsx';
+import { launchImageLibrary } from 'react-native-image-picker';
+import { handleCameraPermission } from '../../Apppermissions/checkPerimissions.ts';
 
 
 export const CanteenMenuScreen = () => {
+  const image = "https://www.w3schools.com/howto/img_avatar.png"
   const [menuItems, SetMenuItems] = useState<menuSection[]>(DATA)
+  const [profileimage, setProfileimage] = useState<any>(image)
   const handleDelete = (sectionTitle: string, id: string) => {
     const updatedSections = menuItems.map(section => {
       if (section.title === sectionTitle) {
@@ -22,8 +26,21 @@ export const CanteenMenuScreen = () => {
     });
     SetMenuItems(updatedSections);
   }
+  const handleProfilePress = async () => {
+    const permission = await handleCameraPermission();
+    if (permission === 'granted' || permission === 'limited') {
+      launchImageLibrary({ mediaType: 'photo' }, (response) => {
+        setProfileimage(response.assets?.at(0)?.uri);
+      });
+    } else {
+      Alert.alert('Permission denied', 'Cannot change profile picture.');
+    }
+  };
+
   return (
     <SafeAreaView style={styles.container} edges={['top']}>
+    <Pressable style={styles.profileContainer} onPress={handleProfilePress}>
+        {profileimage && <Image style={styles.profile} src={profileimage} />}</Pressable>
       <Text style={styles.tittle}>Everest-Canteen👨‍🍳</Text>
       <SectionList
         sections={menuItems}
