d354f1f2aa68b54e7d40c70d3746b38237b22a12
chaitanya <chaitanya.rongali@everest.engineering>
2025-11-14 01:07:40 +0530

    Add styles for the place order button

---
+++ src/screens/orders/OrdersScreen.ts
@@ -0,0 +1,19 @@
+import { StyleSheet } from "react-native";
+export const styles = StyleSheet.create(
+    {
+        button: {
+            backgroundColor: ' hsla(44, 96%, 52%, 1.00)',
+            borderRadius: 25,
+            color: "white",
+            paddingVertical: 15,
+            paddingHorizontal: 32,
+            textAlign: 'center',
+            fontSize: 30,
+            cursor: "pointer",
+            width: "40%",
+            height: 50,
+            margin: 14,
+        }
+
+    }
+)
