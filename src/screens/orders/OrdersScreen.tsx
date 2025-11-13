eff22a7579e6d8f2ac171098d4fd6c8a6f9a340c
chaitanya <chaitanya.rongali@everest.engineering>
2025-11-14 01:06:35 +0530

    Add notification functionality

--- src/screens/orders/OrdersScreen.tsx
+++ src/screens/orders/OrdersScreen.tsx
@@ -1,9 +1,31 @@
-import { Text } from "react-native"
+import { Platform, Text } from "react-native"
+import { CustomButton } from "../../components/Button/CustomButton"
+import { styles } from "./OrdersScreen"
+import notifee from '@notifee/react-native';
+import { RouteProp } from "@react-navigation/native";
+type OrdersScreenRouteParams = {
+    userName: string;
+    admin: string;
+    user: string;
+};
+type OrdersScreenProps = {
+    route: RouteProp<{ OrdersScreen: OrdersScreenRouteParams }, "OrdersScreen">;
+};
+export const OrdersScreen: React.FC<OrdersScreenProps> = ({ route }) => {
+    const { userName, admin, user } = route.params;
 
-export const OrdersScreen = () => {
+    async function displayNotification(name: string) {
+        
+        await notifee.requestPermission()
+        await notifee.displayNotification({
+            title: 'Canteen App',
+            body: `Hi ${name}  your order got placed!!!`,
+        });
+    }
     return (
         <>
             <Text>Display Orders!</Text>
+            {user === "user" && (<CustomButton onPress={() => { displayNotification(userName) }} title={"Place Order"} style={styles.button} />)}
         </>
     )
 }
