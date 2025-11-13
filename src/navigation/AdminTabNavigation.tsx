1f23e1c7c51f3fda759b63e717e189f66b6ef445
chaitanya <chaitanya.rongali@everest.engineering>
2025-11-14 01:06:12 +0530

    Add role props and static username props

--- src/navigation/AdminTabNavigation.tsx
+++ src/navigation/AdminTabNavigation.tsx
@@ -2,13 +2,17 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { NavigationContainer } from "@react-navigation/native";
 import { CanteenMenuScreen } from "../screens/canteenMenu/CanteenMenu.tsx";
 import { OrdersScreen } from "../screens/orders/OrdersScreen.tsx";
+export type AdninTabParamList = {
+  CanteenMenuScreen: () => React.JSX.Element
+  OrdersScreen: { userName: string,admin:string, user:string  };
+};
 
-const Tab = createBottomTabNavigator();
+const Tab = createBottomTabNavigator<AdninTabParamList>();
 export default function AdminTabNavigation() {
     return (
         <Tab.Navigator screenOptions={{ headerShown: false }}>
-            <Tab.Screen name="CanteenMenu" component={CanteenMenuScreen} />
-            <Tab.Screen name="Orders" component={OrdersScreen} />
+            <Tab.Screen name="CanteenMenuScreen" component={CanteenMenuScreen} />
+            <Tab.Screen name="OrdersScreen" component={OrdersScreen} initialParams={{ userName: "Chaitanya",admin:"admin",user:"user" }} />
         </Tab.Navigator>
 
     );
--- src/navigation/UserTabNavigation.tsx
+++ src/navigation/UserTabNavigation.tsx
@@ -1,13 +1,19 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import UserMenuScreen from "../screens/UserMenuScreen/UserMenuScreen";
-import { OrdersScreen } from "../screens/orders/OrdersScreen";
+import { OrdersScreen } from "../screens/orders/OrdersScreen.tsx";
 
-const Tab = createBottomTabNavigator();
+export type UserTabParamList = {
+  UserMenuScreen: () => React.JSX.Element
+  OrdersScreen: { userName: string,admin:string, user:string};
+};
+
+
+const Tab = createBottomTabNavigator<UserTabParamList>();
 export const UserTabNavigation = () => {
     return (
         <Tab.Navigator screenOptions={{ headerShown: false }}>
             <Tab.Screen name="UserMenuScreen" component={UserMenuScreen} />
-            <Tab.Screen name="OrdersScreen" component={OrdersScreen} />
+            <Tab.Screen name="OrdersScreen" component={OrdersScreen}  initialParams={{ userName: "Chaitanya",admin:"admin",user:"user" }}/>
         </Tab.Navigator>
 
     );
